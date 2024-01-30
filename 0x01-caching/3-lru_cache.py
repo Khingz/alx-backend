11c86c50be3095f569de0e19c216c1aef707ea8f
khingz <kingsley995mr@gmail.com>
2024-01-30 19:28:24 +0000

    forst push

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 """Comment"""
-from 
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Comment"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+        self.get_count = {}
+        
+    def put(self, key, item):
+        """Put method"""
+        pass
+
+    def get(self, key):
+        """get a calue from dict"""
+        result = self.cache_data.get(key, None)
+        print(result)
+        if result is not None:
+            pass
+        return result
