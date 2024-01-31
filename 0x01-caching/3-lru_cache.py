8279ffa78d8563ad7bd71c3b915b8751b47757e2
khingz <kingsley995mr@gmail.com>
2024-01-31 10:21:31 +0000

    task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
-"""Comment"""
+"""Module for LRU caching
+"""
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """Comment"""
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached
+    """
     def __init__(self):
-        """init method"""
+        """init method
+        """
         super().__init__()
-        self.get_count = {}
-        
+        self.used_list = []
+
     def put(self, key, item):
-        """Put method"""
-        pass
+        """Put method
+        """
+        if key is not None and item is not None:
+            exist = self.cache_data.get(key, None)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and not exist:
+                pop_key = self.used_list[0]
+                self.cache_data.pop(pop_key)
+                self.used_list.remove(pop_key)
+                print("DISCARD: {}".format(pop_key))
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and exist:
+                self.used_list.remove(key)
+            self.cache_data[key] = item
+            self.used_list.append(key)
 
     def get(self, key):
-        """get a calue from dict"""
+        """get a calue from dict
+        """
         result = self.cache_data.get(key, None)
-        print(result)
         if result is not None:
-            pass
+            self.used_list.remove(key)
+            self.used_list.append(key)
         return result
