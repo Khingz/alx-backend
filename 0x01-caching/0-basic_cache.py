33c0f4ac479c0616cdfa79a412a867b5ef0716a1
khingz <kingsley995mr@gmail.com>
2024-01-31 08:14:34 +0000

    task failing

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Comment
+"""Basic caching module.
 """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Simple caching class
-    second line
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
     """
     def put(self, key, item):
         """Insert to cache
@@ -16,6 +16,6 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """get a calue from dict
+        """get a value from dict
         """
         return self.cache_data.get(key, None)
